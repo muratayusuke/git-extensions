1a42a61893ec76572d9ff5839bba0008f01d4bfe
muratayusuke <info@muratayusuke.com>
2012-10-30 21:59:54 +0900

    change directory structure and installer

--- installer.sh
+++ installer.sh
@@ -1,1 +1,15 @@
-install -v -m 0755 git-ffcheck /usr/local/bin/
+mkdir -p ~/.git-extensions/bin
+install -v -m 0755 ./bin/* ~/.git-extensions/bin/
+shell=`echo ${SHELL} | awk -F/ '{ print $NF }'`
+
+echo "================================"
+echo " install completed"
+echo "================================"
+echo
+echo "Please add the line to ~/.${shell}rc :"
+echo
+echo "\t[[ -s ~/.git-extensions/scripts/init ]] && source ~/.git-extensions/scripts/init"
+echo
+echo "You need to run 'source ~/.${shell}rc' before you can start using git-extensions."
+echo
+echo "================================"
